56f3a45a1bd7ff50d9cca1fb96452c1868e083b4
Imdat Solak <imdat@solak.de>
2022-04-13 08:25:50 +0200

    Removed you_complete_me -- too complicated to intall on linux and not worth it

--- install_deps.sh
+++ install_deps.sh
@@ -2,3 +2,4 @@
 brew install node npm yarn ctags universal-ctags pyvim silver-search ag lsd neovim ranger cowsay fortune cocoapods lazygit cmake python mono go nodejs zoxide
 brew tap yoheimuta/protolint
 brew install protolint
+pip3 install pynvim
--- youcompleteme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-open https://vimawesome.com/plugin/youcompleteme
-cd ~/.vim/plugged/youcompleteme
-python3 install.py
